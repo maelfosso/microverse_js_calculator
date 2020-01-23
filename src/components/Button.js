07d7b7872d54bab3ae5a134a8ad5a191456b6af5
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-23 22:18:34 +0100

    Style the Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 function Button(props) {
   const { name } = props;
-  return <div>{name}</div>;
+  return <div className="Button">{name}</div>;
 }
 
 Button.propTypes = {
