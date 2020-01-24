2391fc80fdcb544d809230427808df9f25899e68
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-24 19:11:39 +0100

    Eslint error fixed

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,46 +4,41 @@ const calculate = (data, buttonName) => {
   const { total, next, operation } = data;
   const digits = [...'0123456789.'];
   const biops = [...'%÷X-+'];
-  const unops = ['%', '+/-']
+  const unops = ['%', '+/-'];
 
   if (digits.includes(buttonName)) {
     return {
       total,
       next: next + buttonName,
-      operation
-    }
-  } else {
-    if (biops.includes(buttonName)) {
-      return {
-        total,
-        next,
-        operation: buttonName
-      }
-    } else {
-      if (unops.includes(buttonName)) {
-        return {
-          total: operate(total, 1, buttonName),
-          next: '',
-          operation: buttonName
-        }
-      } else {
-        if (buttonName == 'AC') {
-          return {
-            total: null,
-            next: null,
-            operation: null
-          }
-        } else {
-          return {
-            total: operate(total, next, buttonName),
-            next: '',
-            operation: buttonName
-          }
-        }
-      }
-    }
+      operation,
+    };
   }
-  
+  if (biops.includes(buttonName)) {
+    return {
+      total,
+      next,
+      operation: buttonName,
+    };
+  }
+  if (unops.includes(buttonName)) {
+    return {
+      total: operate(total, 1, buttonName),
+      next: '',
+      operation: buttonName,
+    };
+  }
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+  return {
+    total: operate(total, next, buttonName),
+    next: '',
+    operation: buttonName,
+  };
 };
 
 export default calculate;
