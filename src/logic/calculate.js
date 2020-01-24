44edc99f267b0f1316f5bc2cd525c015e74a9dc3
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-24 09:57:41 +0100

    operates implemented

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,31 +1,8 @@
-import Big from './big.mjs';
+import operate from './operate';
 
 const calculate = (data, buttonName) => {
   const { total, next, operation } = data;
-  let x = Big(total);
-  let y = Big(next);
-  switch (buttonName) {
-    case '+/-':
-      return x.times(y).times(-1);
-
-    case '%':
-      return x.mod(next);
-
-    case '÷':
-      return x.div(next);
-
-    case 'X':
-      return x.times(next);
-
-    case '-':
-      return x.minus(next);
-
-    case '+':
-      return x.add(next);
-
-    default:
-      return null;
-  }
+  return operate(total, next, buttonName);
 }
 
 export default calculate;
