d016a2ed498667f7a053634001f700eafbfd2d8c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-24 19:09:05 +0100

    Better understanding of calculate

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,49 @@
 import operate from './operate';
 
 const calculate = (data, buttonName) => {
-  const { total, next } = data;
-  return operate(total, next, buttonName);
+  const { total, next, operation } = data;
+  const digits = [...'0123456789.'];
+  const biops = [...'%÷X-+'];
+  const unops = ['%', '+/-']
+
+  if (digits.includes(buttonName)) {
+    return {
+      total,
+      next: next + buttonName,
+      operation
+    }
+  } else {
+    if (biops.includes(buttonName)) {
+      return {
+        total,
+        next,
+        operation: buttonName
+      }
+    } else {
+      if (unops.includes(buttonName)) {
+        return {
+          total: operate(total, 1, buttonName),
+          next: '',
+          operation: buttonName
+        }
+      } else {
+        if (buttonName == 'AC') {
+          return {
+            total: null,
+            next: null,
+            operation: null
+          }
+        } else {
+          return {
+            total: operate(total, next, buttonName),
+            next: '',
+            operation: buttonName
+          }
+        }
+      }
+    }
+  }
+  
 };
 
 export default calculate;
