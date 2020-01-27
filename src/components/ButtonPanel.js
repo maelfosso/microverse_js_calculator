ac133c54459fe5ef23d37eaae3ce95e42e6a07c1
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-27 10:46:49 +0100

    Eslint error fixed

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,18 +6,14 @@ import '../App.css';
 const ButtonPanel = (props) => {
   const { clickHandler } = props;
   const buttons = [
-    [{name:'AC', color: 'lg'},{name:'+/-', color: 'lg'},{name:'%', color: 'lg'},{name:'÷'}],
-    [{name:'7', color: 'lg'},{name:'8', color: 'lg'},{name:'9', color: 'lg'},{name:'X'}],
-    [{name:'4', color: 'lg'},{name:'5', color: 'lg'},{name:'6', color: 'lg'},{name:'-'}],
-    [{name:'0', color: 'lg', wide: true},{name:'.', color: 'lg'},{name:'='}],
-  ]
+    [{ name: 'AC', color: 'lg' }, { name: '+/-', color: 'lg' }, { name: '%', color: 'lg' }, { name: '÷' }],
+    [{ name: '7', color: 'lg' }, { name: '8', color: 'lg' }, { name: '9', color: 'lg' }, { name: 'X' }],
+    [{ name: '4', color: 'lg' }, { name: '5', color: 'lg' }, { name: '6', color: 'lg' }, { name: '-' }],
+    [{ name: '0', color: 'lg', wide: true }, { name: '.', color: 'lg' }, { name: '=' }],
+  ];
+  const renderButton = (arr) => arr.map(b => <Button key={b.name} name={b.name} wide={b.wide && true} color={b.color && 'lightgrey'} clickHandler={clickHandler} />);
 
-  const renderPanel = () => {
-    return buttons.map((bs, i) => <div id={'group-' + i}>{renderButton(bs)}</div>)
-  }
-  const renderButton = (arr) => {
-    return arr.map(b => <Button name={b.name} wide={b.wide && true} color={b.color && "lightgrey"} clickHandler={clickHandler} />);
-  }
+  const renderPanel = () => buttons.map(bs => <div key="bs-panel">{renderButton(bs)}</div>);
 
   return <div id="button-panel" className="ButtonPanel">{renderPanel()}</div>;
 };
