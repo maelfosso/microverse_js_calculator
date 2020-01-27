5cec767de53b9b1457d5714327ee7695e2ae5ada
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-01-27 10:36:49 +0100

    Refactoring of ButtonPanel - DRY

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,41 +5,21 @@ import '../App.css';
 
 const ButtonPanel = (props) => {
   const { clickHandler } = props;
+  const buttons = [
+    [{name:'AC', color: 'lg'},{name:'+/-', color: 'lg'},{name:'%', color: 'lg'},{name:'÷'}],
+    [{name:'7', color: 'lg'},{name:'8', color: 'lg'},{name:'9', color: 'lg'},{name:'X'}],
+    [{name:'4', color: 'lg'},{name:'5', color: 'lg'},{name:'6', color: 'lg'},{name:'-'}],
+    [{name:'0', color: 'lg', wide: true},{name:'.', color: 'lg'},{name:'='}],
+  ]
 
+  const renderPanel = () => {
+    return buttons.map((bs, i) => <div id={'group-' + i}>{renderButton(bs)}</div>)
+  }
+  const renderButton = (arr) => {
+    return arr.map(b => <Button name={b.name} wide={b.wide && true} color={b.color && "lightgrey"} clickHandler={clickHandler} />);
+  }
 
-  return (
-    <div id="button-panel" className="ButtonPanel">
-      <div id="group-1">
-        <Button name="AC" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="+/-" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="%" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="÷" clickHandler={clickHandler} />
-      </div>
-      <div id="group-2">
-        <Button name="7" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="8" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="9" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="X" clickHandler={clickHandler} />
-      </div>
-      <div id="group-3">
-        <Button name="4" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="5" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="6" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="-" clickHandler={clickHandler} />
-      </div>
-      <div id="group-4">
-        <Button name="1" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="2" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="3" color="lightgrey" clickHandler={clickHandler} />
-        <Button name="+" clickHandler={clickHandler} />
-      </div>
-      <div id="group-5">
-        <Button name="0" wide color="lightgrey" clickHandler={clickHandler} />
-        <Button name="." color="lightgrey" clickHandler={clickHandler} />
-        <Button name="=" clickHandler={clickHandler} />
-      </div>
-    </div>
-  );
+  return <div id="button-panel" className="ButtonPanel">{renderPanel()}</div>;
 };
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
